cbe7c2ef20cfc8f33917f7320deb6794bd3bc0dc
jasondoan <doananhtuan22111996@gmail.com>
2024-09-01 01:28:37 +0700

    feat: Create Android Core

---
+++ domain/src/main/java/vn/core/usecase/BaseUseCase.kt
@@ -0,0 +1,7 @@
+package vn.core.usecase
+
+import kotlinx.coroutines.flow.Flow
+
+abstract class BaseUseCase<in Request : Any, out Result> {
+    abstract fun execute(vararg params: Request?): Flow<Result>
+}
